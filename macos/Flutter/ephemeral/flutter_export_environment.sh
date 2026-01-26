a6ba2e89bc5d6936d89ec643c2cd89a1e428e28a
DELL <mraed225588@gmail.com>
2026-01-27 00:50:47 +0300

    sdk

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\hp\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\hp\StudioProjects\najaz\safirah"
+export "FLUTTER_ROOT=C:\Users\DELL\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\DELL\StudioProjects\safirah"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
